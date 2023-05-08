4624f84a5bd7bc06eb3e9b88b77c142f4fdfc944
bobur2006coder <boburdilmurodov702@gmail.com>
2023-05-08 20:24:13 +0500

    first commit

---
+++ javascript.js
@@ -0,0 +1,37 @@
+// 3 saloni javobi 
+
+let  a = prompt("yozing")
+
+if (a == "hello world" || a== "wor") {
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+
+
+
+
+
+
+// 4 savolni javobi
+
+let obj = {
+    Name: "John Doe: ",
+    email: "john.doe@example.com"
+}
+
+
+console.log(obj.Name,obj.email);
+
+
+
+
+// 5 savolni javobi 
+
+let a = ["hello" , "salom", "tamom"]
+
+if () {
+    
+}
